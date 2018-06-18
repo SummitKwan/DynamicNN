1c9b1dd644d9c0e5013c4bea7c71d02ae29dca25
SummitKwan <SummitKwan@hotmail.com>
2018-06-18 10:32:05 -0400

    rbm

--- rbm.py
+++ rbm.py
@@ -1,7 +1,7 @@
 """
-Boltzmann machines, fully observed, no hidden units, implemented in numpy
+Restricted Boltzmann machines,  implemented in numpy
 Shaobo Guan,
-2018-0517
+2018-0813
 """
 
 import os
@@ -12,7 +12,7 @@ import h5py
 import pickle
 
 
-modelname = 'BoltzmannNetNoHidden'
+modelname = 'RBM'
 
 def sigmoid_fun(z):
     """ sigmoid activation function, does not change the shape is input z """
@@ -28,80 +28,127 @@ def remove_diagonal(W):
     return W * (1 - np.identity(W.shape[0]))
 
 
-class BoltzmannNetNoHidden:
+class RestrictedBoltzmannMachine:
 
-    def __init__(self, m):
+    def __init__(self, m1, m2):
         """
         initialize model
         :param m:  number of nodes in network
         """
-        self.parameters = ('m', 'W', 'b')
-        self.m = m
-        self.b = np.random.randn(*[1, m]) * 0.001
-        self.W = np.random.randn(*[1, m]) * 0.001
-        self.W = remove_diagonal(self.W + self.W.transpose())
+        self.parameters = ('m1', 'm2', 'W', 'b1', 'b2')
+        self.m1 = m1
+        self.m2 = m2
+        self.b1 = np.random.randn(*[1, m1]) * 0.001
+        self.b2 = np.random.randn(*[1, m2]) * 0.001
+        self.W = np.random.randn(*[m1, m2]) * 0.001
 
 
-    def cal_energy(self, X):
+    def cal_energy(self, X, Y=None):
         """
         compute energy
         :param X:  data, shape=[num_data, num_neurons]
         :return:   energy of hopfield model, shape=[num_data]
         """
-        energy = -np.sum(X*self.b + np.matmul(X, self.W) * X / 2, axis=1)
+        if Y is None:
+            Y = self.compute_p_y_given_x(X)
+        energy = -np.sum(X*self.b1,  axis=1) - np.sum(Y*self.b2, axis=1) \
+                 - np.sum(X[:, :] * np.sum(Y[:, None, :] * self.W[None, :, :], axis=2), axis=1)
         return energy
 
 
-    def inference(self, X, mask_update=None, num_steps=1, I=None):
+    def compute_p_y_given_x(self, X):
+        """
+        inference of Y based on X
+
+        :param X: data, shape=[num_data, m1]
+        :return:  updated Y, shape=[num_data, m2]
+        """
+        return sigmoid_fun(np.sum(X[:, :, None] * self.W[None, :, :], axis=1) + self.b2 )
+
+
+    def compute_p_x_given_y(self, Y):
+        """
+        inference of Y based on X
+
+        :param Y: data, shape=[num_data, m2]
+        :return:  updated X, shape=[num_data, m1]
+        """
+        return sigmoid_fun(np.sum(Y[:, None, :] * self.W[None, :, :], axis=2) + self.b1)
+
+
+    def inference(self, X=None, Y=None, mask_update=None, num_steps=1, direction='auto'):
         """
         evolve to a steady state using MCMC
         :param X:  data, shape=[num_data, num_neurons]
         :param mask_update:  binary masks of whether to allow updating every pixel, shape=[num_data, num_neurons]
         :param num_steps: number of steps to run the sampling process over the network
-        :param I: external input
-        :return:   updated sample of X
+        :param direction: one of ['XY', 'YX', 'auto'].  X->Y or Y->X, default to 'auto', determined by whether X or Y is given
+        :return:
         """
-        n, m = X.shape
-        X = X + 0
-        if I is None:
-            I = self.b * 0
-        if mask_update is None:
-            mask_update = np.ones(X.shape).astype('bool')
-        for iter in range(num_steps):
-            for mm in np.random.permutation(m):
-                Xmm = bernoulli_sample(sigmoid_fun(np.matmul(X, self.W[:, mm]) + self.b[:, mm] + I))
-                X[:, mm] = Xmm * mask_update[:, mm] + X[:, mm] * (1 - mask_update[:, mm])
 
-        return X
+        assert (X is not None) or (Y is not None)
+        assert direction in ['XY', 'YX', 'auto']
+        if direction == 'auto':
+            if (X is not None) and (Y is None):
+                direction = 'XY'
+            elif (X is None) and (Y is not None):
+                direction = 'YX'
+            else:
+                direction = 'XY'
+        pX = None
+        pY = None
+
+        # if mask_update is None:
+        #     mask_update = np.ones(X.shape).astype('bool')
+
+        for iter in range(num_steps):
+            if direction == 'XY':
+                pY = self.compute_p_y_given_x(X)
+                Y = bernoulli_sample(pY)
+                direction = 'YX'
+            elif direction == 'YX':
+                pX = self.compute_p_x_given_y(Y)
+                X = bernoulli_sample(pX)
+                direction = 'XY'
+
+        return (X, Y), (pX, pY)
 
 
     def train(self, X, lr=0.001, steps_negstats=1, batchsize=50, yn_verbose=False, batches_per_print=100):
-        """ training using exponential family's gradient formula """
-        n, m = X.shape
+        """ training using contrastive divergence algorithm, wrapper of self.train_batch() """
+        n = X.shape[0]
         num_batch = n//batchsize
         tic = time.time()
         # self.train_cost = {t: []; cost: []}
         for i_batch in range(num_batch):
-            self.train_batch(X[i_batch*batchsize : i_batch*batchsize+batchsize], lr=lr, steps_negstats=steps_negstats)
+            X_batch = X[i_batch * batchsize: i_batch * batchsize + batchsize]
+            self.train_batch(X_batch, lr=lr, steps_negstats=steps_negstats)
             if yn_verbose and i_batch%batches_per_print == 0:
                 toc = time.time()
-                print('batch {}/{}, time total={:.0f} sec'.format(i_batch, num_batch, toc-tic))
+                energy_batch = np.mean(self.cal_energy(X_batch))
+                print('batch {}/{}, time total={:.0f} sec, energy_tr={}'.format(i_batch, num_batch, toc-tic, energy_batch))
 
 
     def train_batch(self, X, lr=0.001, steps_negstats=1):
-        """ training using exponential family's gradient formula """
-        n, m = X.shape
-        X_sample = self.inference(X, num_steps=steps_negstats)
+        """ training using contrastive divergence algorithm """
 
-        stats_pos_uni = np.mean(X, axis=0, keepdims=True)
-        stats_pos_bin = np.matmul(X.transpose(), X) * 1.0 / n
+        X_pos = X
+        (_, Y_pos), (_, pY_pos) = self.inference(X=X_pos, num_steps=1)
+        (X_neg, Y_neg), (pX_neg, pY_neg) = self.inference(Y=Y_pos, num_steps=steps_negstats*2)
 
-        stats_neg_uni = np.mean(X_sample, axis=0, keepdims=True)
-        stats_neg_bin = np.matmul(X_sample.transpose(), X_sample) * 1.0 / n
 
-        self.b += lr * (stats_pos_uni - stats_neg_uni)
-        self.W += lr * (stats_pos_bin - stats_neg_bin)
-        self.W = remove_diagonal(self.W)
+        stats_X_pos = np.mean( X_pos, axis=0, keepdims=True)
+        stats_Y_pos = np.mean(pY_pos, axis=0, keepdims=True)
+        stats_XY_pos = np.mean(X_pos[:, :, None] * pY_pos[:, None, :], axis=0)
+
+        stats_X_neg = np.mean(pX_neg, axis=0, keepdims=True)
+        stats_Y_neg = np.mean(pY_neg, axis=0, keepdims=True)
+        stats_XY_neg = np.mean(X_neg[:, :, None] * pY_neg[:, None, :], axis=0)
+
+        self.b1 += lr * (stats_X_pos - stats_X_neg)
+        self.b2 += lr * (stats_Y_pos - stats_Y_neg)
+        self.W += lr * (stats_XY_pos - stats_XY_neg)
+
 
 
     def save_parameters(self, filepathname=None,
@@ -157,6 +204,7 @@ class BoltzmannNetNoHidden:
         :return:
         """
 
+        # todo: compatability
         if filepathname is not None:
             filedir, basename = os.path.split(filepathname)
             filename, fileext = os.path.splitext(basename)
@@ -189,4 +237,3 @@ class BoltzmannNetNoHidden:
                     setattr(self, key, data_dict[key])
         else:
             raise Exception('fileext must be one of (".h5", ".pkl")')
-
